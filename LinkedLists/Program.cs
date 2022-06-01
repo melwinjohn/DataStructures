561ce15626c86de6a8e95de2de8650377993edbd
melwin_extc <melwin_extc@yahoo.co.in>
2022-06-01 13:18:41 -0700

    Add project files.

---
+++ LinkedLists/Program.cs
@@ -0,0 +1,19 @@
+﻿//main method
+
+static void Main(string[] args)
+{
+    Node p = new Node(6);    
+}
+
+public class Node
+{
+    int data;
+    Node next;
+    public Node(int d)
+    {
+        this.data = d;
+    }     
+
+}
+
+
